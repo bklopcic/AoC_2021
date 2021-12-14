879a086486438f4ce2c16cd0b7700de27236bee5
Benjamin Klopcic <benjaminklopcic@gmail.com>
2021-12-14 00:55:52 -0500

    some stuff

--- day_14/pt1main.js
+++ day_14/pt1main.js
@@ -1,7 +1,7 @@
 const utils = require('../utilities/js/Utils.js');
 
-//let content = utils.getLineDelimitedFileContent('./sample.txt');
-let content = utils.getLineDelimitedFileContent('./input.txt');
+let content = utils.getLineDelimitedFileContent('./sample.txt');
+//let content = utils.getLineDelimitedFileContent('./input.txt');
 
 let sequence = content[0];
 
@@ -14,13 +14,52 @@ let instructions = content.splice(2, content.length).map( o => {
     }
 });
 
-for (let i = 0; i < 100; i++)
+console.log(sequence);
+console.log(instructions);
+for (let i = 0; i < 10; i++)
 {
     let arr = sequence.split('').map(o => { return {char: o, include: true}});
 
     for (let inst of instructions)
     {
-
+        for (let  j = 0; j < arr.length-1; j++)
+        {
+            if (arr[j].include && arr[j].char == inst.start && arr.slice(j+1, arr.length).find( o => o.include).char == inst.end)
+            {
+                arr.splice(j+1, 0, {char: inst.insert, include: false});
+                j++;
+            }
+        }
     }
+    sequence = arr.map(o => o.char).join('');
 }
 
+console.log(sequence);
+console.log(maxCount(sequence));
+console.log(minCount(sequence));
+
+function maxCount(input) {
+    const {max, ...counts} = (input || "").split("").reduce(
+    (a, c) => {
+        a[c] = a[c] ? a[c] + 1 : 1;
+        a.max = a.max < a[c] ? a[c] : a.max;
+        return a;
+    },
+    { max: 0 }
+    );
+
+    return Object.entries(counts).filter(([k, v]) => v === max);
+}
+
+function minCount(input) {
+    const {min, ...counts} = (input || "").split("").reduce(
+    (a, c) => {
+        a[c] = a[c] ? a[c] + 1 : 1;
+        a.min = a.min > a[c] ? a[c] : a.min;
+        return a;
+    },
+    { min: 0 }
+    );
+    
+    return Object.entries(counts).filter(([k, v]) => v === min);
+}
